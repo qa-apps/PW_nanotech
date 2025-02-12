00cdea355cd65253e3ce0012d30e75faabdcc4f8
Alex Pavlovsky <pasok.a@yahoo.com>
2025-02-12 12:00:00 -0500

    Remove placeholder example spec

--- tests/example.spec.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('Playwright docs landing page has expected title', async ({ page }) => {
-  await page.goto('/');
-  await expect(page).toHaveTitle(/Playwright/);
-});
